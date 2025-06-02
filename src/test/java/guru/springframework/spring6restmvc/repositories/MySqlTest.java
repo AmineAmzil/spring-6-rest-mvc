89d9f6f8d7651d1288e677c60cb78a85cf63ea56
Amine Amzil <official.amzil@gmail.com>
2025-06-02 03:24:44 +0200

    use ServiceConnection to map properties

--- src/test/java/guru/springframework/spring6restmvc/repositories/MySqlTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/MySqlTest.java
@@ -1,17 +1,18 @@
 package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,23 +26,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MySqlTest {
 
     @Container
+    @ServiceConnection
     static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16-alpine");
     @Autowired
     DataSource dataSource;
     @Autowired
     BeerRepository beerRepository;
 
-    @DynamicPropertySource
-    static void mySqlProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
-        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
-        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
-    }
-
     @Test
     void testListBeers() {
+        beerRepository.saveAllAndFlush(
+                List.of(
+                        Beer.builder()
+                                .beerName("Milk")
+                                .quantityOnHand(10)
+                                .upc("Milk")
+                                .beerStyle(BeerStyle.GOSE)
+                                .price(new BigDecimal("19.99"))
+                                .version(1)
+                                .build(),
+                        Beer.builder()
+                                .beerName("Test 02")
+                                .quantityOnHand(3)
+                                .upc("Test 02")
+                                .beerStyle(BeerStyle.LAGER)
+                                .price(new BigDecimal("9.99"))
+                                .version(1)
+                                .build()
+                )
+        );
+
         List<Beer> beers = beerRepository.findAll();
 
-        assertThat(beers.size()).isGreaterThan(0);
+        assertThat(beers.size()).isGreaterThan(2);
     }
 }
