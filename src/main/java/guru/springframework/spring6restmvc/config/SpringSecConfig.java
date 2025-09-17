188e1990210380df770ced739b836bd0a469931a
Amine Amzil <amine.amzil@leanmind.de>
2025-09-17 02:12:27 +0200

    248: testing with postman

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,12 +14,12 @@ public class SpringSecConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http.csrf(httpSecurityCsrfConfigurer -> {
-            httpSecurityCsrfConfigurer.ignoringRequestMatchers("/api/**");
-        });
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(
+                        auth -> auth.jwt(Customizer.withDefaults())
+                );
 
         return http.build();
     }
-
 }
